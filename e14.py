c9eb6f1153e8e7f65f596976ef6f9256a36b5242
aneva91931 <133877267+aneva91931@users.noreply.github.com>
2023-05-17 21:01:14 +0800

    Add files via upload

---
+++ e14.py
@@ -0,0 +1,28 @@
+month = int(input("请输入一个月份："))
+if month == (2):
+    print("28天")
+elif month == (1):
+    print("31天")
+elif month == (3):
+    print("31天")
+elif month == (5):
+    print("31天")
+elif month == (7):
+    print("31天")
+elif month == (8):
+    print("31天")
+elif month == (10):
+    print("31天")
+elif month == (12):
+    print("31天")
+elif month == (4):
+    print("30天")
+elif month == (6):
+    print("30天")
+elif month == (9):
+    print("30天")
+elif month == (11):
+    print("30天")
+
+else:
+    print("无效月份")
